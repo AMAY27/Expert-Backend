aa0993f91fb339a2dbfc7bb15b444ba4ae891e0f
Amay Rajvaidya <amay.rajvaidya@s2022.tu-chemnitz.de>
2024-03-09 02:12:40 +0530

    CORS policy updated

--- src/main.ts
+++ src/main.ts
@@ -22,9 +22,13 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService);
   app.enableCors({
-    allowedHeaders: ['content-type'],
-    origin: 'http://localhost:3000',
-    credentials: true,
+    origin: 'http://localhost:3000', // Allow requests from this origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // Allow these HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
+    credentials: true, // Allow sending credentials (e.g., cookies, authorization headers)
+    preflightContinue: false, // Disable preflight requests cache
+    optionsSuccessStatus: 200, // Response status for successful OPTIONS requests
+    maxAge: 3600, // How long the results of a preflight request can be cached in seconds
   });
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
