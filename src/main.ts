8cb4116bea507bc4a5faea7df9735722fec1b0a2
Amay Rajvaidya <amay.rajvaidya@s2022.tu-chemnitz.de>
2024-03-09 03:07:10 +0530

    PORT for railway added

--- src/main.ts
+++ src/main.ts
@@ -21,19 +21,11 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   const configService = app.get(ConfigService);
-  app.enableCors({
-    origin: 'http://localhost:3000', // Allow requests from this origin
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // Allow these HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
-    credentials: true, // Allow sending credentials (e.g., cookies, authorization headers)
-    preflightContinue: false, // Disable preflight requests cache
-    optionsSuccessStatus: 200, // Response status for successful OPTIONS requests
-    maxAge: 3600, // How long the results of a preflight request can be cached in seconds
-  });
+  app.enableCors();
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api-docs', app, document);
 
-  await app.listen(configService.get<number>('APP_PORT'));
+  await app.listen(process.env.PORT || configService.get<number>('APP_PORT'));
 }
 bootstrap();
