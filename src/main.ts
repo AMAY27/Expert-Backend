fa11302aaa7afb1d59bc5de6103a7d4e9eaae112
Amay Rajvaidya <amay.rajvaidya@s2022.tu-chemnitz.de>
2024-03-11 05:50:54 +0530

    dotenv config updated

--- src/main.ts
+++ src/main.ts
@@ -9,13 +9,13 @@ import * as process from 'process';
 
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
-  const nodeEnv = process.env.NODE_ENV || '';
+  const nodeEnv = process.env.NODE_ENV || 'prod';
   const envFilePath = `.env.${nodeEnv}`;
   logger.log(
     `Selected environment: ${nodeEnv} and Selected env file path: ${envFilePath}`,
   );
 
-  dotenv.config({ path: envFilePath });
+  dotenv.config();
 
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
